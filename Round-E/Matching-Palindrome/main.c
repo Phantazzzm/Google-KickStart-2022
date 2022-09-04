d67942754eddc5190ff6181b5ca17523ebf90a5a
Phantazzzm <phantazzzm@gmail.com>
2022-09-04 13:29:53 +0300

    Create main.c

---
+++ Round-E/Matching-Palindrome/main.c
@@ -0,0 +1,37 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+main(){
+
+	char t;
+	scanf("%d",&t);
+	
+	for(char c=0;c<t;c++){
+	
+		int n;
+		scanf("%d",&n);
+		
+		char *p=malloc((n+1)*sizeof(char));
+		scanf("%s",p);
+		
+		int qn=0;
+		while(++qn<n)
+			if(n%qn==0){
+				for(int qi=qn;qi<n/2+n%2;qi++)
+					if(p[qi%qn]!=p[qi])
+						goto cqn;
+				for(int qi=0;qi<qn/2;qi++)
+					if(p[qi]!=p[qn-1-qi])
+						goto cqn;
+				break;
+				cqn:continue;
+			}
+			
+		p[qn]=0;
+		printf("Case #%d: %s\n",c+1,p);
+		
+		free(p);
+	
+	}
+
+}
